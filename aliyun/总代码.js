ff32a2dbb99c5322b29047cff5e3457e4d2d228e
1260957445@qq.com <1260957445@qq.com>
2022-02-05 21:15:36 +0800

    gg

--- aliyun/总代码.js
+++ aliyun/总代码.js
@@ -1,12 +1,12 @@
-######分享码
+######分享码1
 if(getVar("url").split(".com/s/")[1].length>3){
     getVar("url").split(".com/s/")[1];
 }else{
     alert("请输入完整的阿里云盘分享链接,比如https://www.aliyundrive.com/s/wUFXj7116uS");
 }
-######share_token
+######share_token2
 JSON.parse(getHttp(JSON.stringify({url:"https://api.aliyundrive.com/v2/share_link/get_share_token",postJson:JSON.stringify({share_pwd:"",share_id:getVar("分享码")})}))).share_token;
-######目录重组数据root
+######目录重组数据root3
 var 目录数据=getHttp(JSON.stringify({url:"https://api.aliyundrive.com/adrive/v3/file/list",head:{"X-Share-Token":getVar("share_token")},postJson:JSON.stringify({share_id:getVar("分享码"),parent_file_id:"root",limit: 100,image_thumbnail_process:"image/resize,w_160/format,jpeg",image_url_process:"image/resize,w_1920/format,jpeg",video_thumbnail_process:"video/snapshot,t_1000,f_jpg,ar_auto,w_300",order_by: "name",order_direction:"ASC"})}));
 var items=JSON.parse(目录数据).items;
 for(var i in items){
@@ -25,7 +25,7 @@ items[i].name="["+items[i].file_extension+"资源文件]"+items[i].name;
 }
 }
 JSON.stringify(items);
-######目录重组数据
+######目录重组数据4
 var file_id=getVar("url").split("$$")[1];
 var share_id=getVar("url").split("$$")[0];
 var 目录数据=getHttp(JSON.stringify({url:"https://api.aliyundrive.com/adrive/v3/file/list",head:{"X-Share-Token":getVar("share_token")},postJson:JSON.stringify({share_id:share_id,parent_file_id:file_id,limit: 100,image_thumbnail_process:"image/resize,w_160/format,jpeg",image_url_process:"image/resize,w_1920/format,jpeg",video_thumbnail_process:"video/snapshot,t_1000,f_jpg,ar_auto,w_300",order_by: "name",order_direction:"ASC"})}));
@@ -46,7 +46,7 @@ items[i].name="["+items[i].file_extension+"资源文件]"+items[i].name;
 }
 }
 JSON.stringify(items);
-######历史记录
+######历史记录5
 eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
 var filename='阿里云历史记录.txt';
 var 记录=[];
@@ -59,11 +59,11 @@ var 新记录=记录.concat(JSON.parse(_.read(filename)||[]));
 var 新记录=记录;
 }
 _.write(JSON.stringify(新记录),filename);
-######读取历史
+######读取历史6
 eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
 var filename='阿里云历史记录.txt';
 _.read(filename);
-######alicookie
+######alicookie7
 var cm=android.webkit.CookieManager.getInstance();
 var ALICOOKIE=cm.getCookie("www.aliyundrive.com");
 if(ALICOOKIE.indexOf("access_token")!=-1){
@@ -76,10 +76,10 @@ JSON.stringify(Alicookie);
 }else{
 alert("请重新登陆阿里云盘网页");
 }
-######过滤非视频
+######过滤非视频8
 var 过滤=JSON.parse(getVar("目录重组数据")).filter(item=>item.category=="video");
 JSON.stringify(过滤);
-######视频地址
+######视频地址9
 var access_token=JSON.parse(getVar("alicookie")).access_token;
 var share_id=getVar("url").split("?wd=")[1].split("$$")[1];
 var file_id=getVar("url").split("?wd=")[1].split("$$")[2];
@@ -91,7 +91,7 @@ alert("登陆已过期，请重新在m浏览器登陆")
 }
 var resp=JZ(JSON.stringify({url:JSON.parse(code).download_url,redirect:false,head:{"Referer":"https://www.aliyundrive.com/"}}));
 JSON.stringify({url:resp.head.location,head:{"Referer":"https://www.aliyundrive.com/"}});
-######文档预览
+######文档预览10
 var cm=android.webkit.CookieManager.getInstance();
 var ALICOOKIE=cm.getCookie("www.aliyundrive.com");
 if(ALICOOKIE.indexOf("access_token")!=-1){
