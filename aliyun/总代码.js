5e0bb72c29d2854f3bcd7362433740aa246bea17
1260957445@qq.com <1260957445@qq.com>
2022-02-07 23:52:58 +0800

    g

--- aliyun/总代码.js
+++ aliyun/总代码.js
@@ -133,7 +133,7 @@ if(getVar("url").indexOf("$$")!=-1){
     if(JSON.parse(code).code){
         alert("登陆已过期，请重新在m浏览器登陆");
     }
-    var resp=JZ(JSON.stringify({url:JSON.parse(code).download_url,redirect:false,head:{"Referer":"https://www.aliyundrive.com/"}}));
+    var resp=JZ(JSON.stringify({url:JSON.parse(code).url,redirect:false,head:{"Referer":"https://www.aliyundrive.com/"}}));
     if(type=="rmvb"){
         _.download({
             url: resp.head.location,
