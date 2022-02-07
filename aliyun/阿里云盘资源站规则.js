471d396d091573aa6f048bd65df23a7122e42a57
1260957445@qq.com <1260957445@qq.com>
2022-02-07 12:16:47 +0800

    gg

--- aliyun/阿里云盘资源站规则.js
+++ aliyun/阿里云盘资源站规则.js
@@ -5,7 +5,7 @@
     "rule":{
         "分类":'var a="影视$https://pan.3636360.com/api/v3/thread.list?perPage=10&page=#PN#&filter[categoryids][0]=1&filter[essence]=$$动漫$https://pan.3636360.com/api/v3/thread.list?perPage=10&page=#PN#&filter[categoryids][0]=7&filter[essence]=$$音乐$https://pan.3636360.com/api/v3/thread.list?perPage=10&page=#PN#&filter[categoryids][0]=2&filter[essence]=";var b="全部$0&filter[attention]=0&filter[sort]=1&scope=0$$推荐$0&filter[attention]=0&filter[sort]=1&scope=1$$精华$1&filter[attention]=0&filter[sort]=1&scope=0$$评论时间$0&filter[attention]=0&filter[sort]=2&scope=0$$热门内容$0&filter[attention]=0&filter[sort]=3&scope=0";a=a.split("$$");b=b.split("$$");var items=[];for(var i in a){var az=a[i].split("$")[0];var ay=a[i].split("$")[1];for(var j in b){var bz=b[j].split("$")[0];var by=b[j].split("$")[1];items.push({title:az+"-"+bz,url:ay+by});}}JSON.stringify(items);',
         "列表规则":'var 列表=e2Arr(getCode(),".json(Data).json(pageData)");var 地址规则=".json(threadId)";var 标题规则=".json(title)";var 图片规则=".json(user).json(avatar)";var 简介规则=".json(content).json(text)";',
-        "详情规则":'var 列表=e2Arr(getVar("CODE"),".z(.+)");var 地址规则=".t().z(https://www.aliyundrive.com/s/[1-9a-zA-Z]+)";var 标题规则=".t()";'
+        "详情规则":'var 列表=e2Arr(getVar("CODE"),".json(detail).z(.+)");var 地址规则=".t().z(https://www.aliyundrive.com/s/[1-9a-zA-Z]+)";var 标题规则=".t()";'
     }
 }
 ]
