04ee5a4025566c83af3f724a353ee7fd15f1fe9e
1260957445@qq.com <1260957445@qq.com>
2022-02-16 00:19:26 +0800

    g

--- aliyun/阿里云盘资源站规则.js
+++ aliyun/阿里云盘资源站规则.js
@@ -16,7 +16,7 @@
 },{
     "title":"TG群组",
     "rule":{
-        "分类":'var a="阿里云盘资源发布$https://tx.me/s/sharealiyun#PN#$$阿里云盘盘$https://tx.me/s/yunpanpan#PN#";var a=a.split("$$");var items=[];for(var i in a){var title=a[i].split("$")[0];var url=a[i].split("$")[1];items.push({title:title,url:url});}JSON.stringify(items);',
+        "分类":'var a="云盘资源发布$https://tx.me/s/sharealiyun#PN#$$云盘盘$https://tx.me/s/yunpanpan#PN#$$云盘影视共享$https://tx.me/s/alypysgx#PN#";var a=a.split("$$");var items=[];for(var i in a){var title=a[i].split("$")[0];var url=a[i].split("$")[1];items.push({title:title,url:url});}JSON.stringify(items);',
         "列表规则":'var 列表=e2Arr(getCode(),".get(div.tgme_widget_message_bubble)");var 地址规则=".get(a.tgme_widget_message_photo_wrap).a(href)";var 标题规则=".c(发表时间:).get(time).a(datetime).c( 标题:).get(div.tgme_widget_message_text).t().z([\\\\s\\\\S]{40})";var 图片规则=".get(a.tgme_widget_message_photo_wrap).a(style).ty(\').tz(\')";var 简介规则=".get(div.tgme_widget_message_text).t()";var NEXTPAGE="https://tx.me"+getCode().match(/rel="prev" href="(.+?)"/)[1];var PREPAGE="https://tx.me"+getCode().match(/rel="canonical" href="(.+?)"/)[1];',
         "详情规则":'var 正文=getVar("msg");'
     }
