077a27b338f018683298eb2018df5edba16690be
1260957445@qq.com <1260957445@qq.com>
2022-02-22 22:58:41 +0800

    g

--- oneiptv/总代码.js
+++ oneiptv/总代码.js
@@ -11,11 +11,11 @@ var 新记录=记录.concat(旧记录);
 var 新记录=记录;
 }
 _.write(新记录.join("\n"),filename);
-"保存成功!";
+_.read(filename);
 }else{
 "请输入正确格式(支持批量):名称,地址";
 }
-######文本写入本地2
+######源文本写入本地2
 eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
 var indexname='一个直播本地索引.txt';
 var 记录=[];
@@ -38,18 +38,102 @@ _.write(新索引.join("\n"),indexname);
 _.write(item.join("\n"),indexname);
 }
 _.write(新记录.join("\n"),filename);
-"保存成功!";
+_.read(indexname)+_.read(filename);
 }else{
-    "请输入正确格式(支持批量):名称,地址";
+"请输入正确格式(支持批量):名称,地址";
 }
-######读取本地订阅3
-eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
-var filename='一个直播本地索引.txt';
-_.read(filename);
-######读取本地文本4
+######读取远程订阅3
 eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
 var filename='一个直播远程索引.txt';
-_.read(filename);
+var code=_.read(filename).match(/.+?,.+/g);
+var items=[];
+for(var i in code){
+var title=code[i].split(",")[0];
+var url="q:自定义播放器?url=远程$$"+code[i].split(",")[1];
+items.push({title:title,url:url});
+}
+JSON.stringify(items);
+######读取本地文本4
+eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
+var filename='一个直播本地索引.txt';
+var code=_.read(filename).match(/.+?,.+/g);
+var items=[];
+for(var i in code){
+var title=code[i].split(",")[0];
+var url="q:自定义播放器?url=本地$$"+code[i].split(",")[1];
+items.push({title:title,url:url});
+}
+JSON.stringify(items);
 ######选集地址5
-
+eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
+var type=getVar("url").split("$$")[0];
+var u=getVar("url").split("$$")[1];
+if(type=="远程"){
+    var code=getHttp(u);
+}else if(type=="本地"){
+    var code=_.read(u);
+}
+function 选集列表(){
+    var res={};var items=[];
+    for(var i=0;i<分类.length;i++){
+        var 分类CODE=分类[i];
+        var 列表=e2Arr(分类CODE,列表规则).filter(Boolean);
+        if(线路){
+        var 标题=e2Rex(线路[i],标题规则);
+        }else{
+        var 标题=e2Rex(分类CODE,标题规则);
+        }
+        var LIST=[];
+        for(var j=0;j<列表.length;j++){
+            var 选集=e2Rex(列表[j],选集规则);
+            var 选集地址=e2Rex(列表[j],选集地址规则);
+            LIST.push({title:选集,url:选集地址});
+        }
+    var play_={};
+    play_.title=标题;
+    play_.list=LIST;
+    items.push(play_);
+    }
+    res.data=items;
+    return JSON.stringify(res);
+}
+if(code.indexOf("#genre#")!=-1){
+    var 分类=code.split(/.+?#genre#.*/).filter(Boolean);
+    var 线路=code.match(/.+?#genre#.*/g);
+    var 列表规则=".z(.+?,.+)";
+    var 标题规则=".tz(#genre#)";
+    var 选集地址规则=".ty(,)";
+    var 选集规则=".tz(,)";选集列表();
+}else if(code.indexOf("#EXTINF:")!=-1){
+    var code=code.match(/#EXTINF:.+[\s]+?.+/g);
+    var head=[];head.push(getVar("name")+"#genre#");
+    code=head.concat(code).join("\n");
+    var 分类=code.split(/.+?#genre#.*/).filter(Boolean);
+    var 线路=code.match(/.+?#genre#.*/g);
+    var 列表规则=".z(#EXTINF:.+[\\s]+?.+)";
+    var 标题规则=".tz(#genre#)";
+    var 选集规则=".z(.+).ty(,)";
+    var 选集地址规则=".c(http://ip111.cn/?wd=).z2(,.+[\\s]+?\\(.+\\))";选集列表();
+}else{
+    var code=code.match(/.+?,.+/g);
+    var head=[];head.push(getVar("name")+"#genre#");
+    code=head.concat(code).join("\n");
+    var 分类=code.split(/.+?#genre#.*/).filter(Boolean);
+    var 线路=code.match(/.+?#genre#.*/g);
+    var 列表规则=".z(.+?,.+)";
+    var 标题规则=".tz(#genre#)";
+    var 选集规则=".tz(,)";
+    var 选集地址规则=".c(http://ip111.cn/?wd=).ty(,)";选集列表();
+}
 ######免嗅探6
+var u=getVar("url").split("/?wd=")[1];
+if(u.indexOf("#")!=-1){
+var list=u.split("#");
+var items=[];
+for(var i=0;i<list.length;i++){
+items.push({url:list[i]});
+}
+JSON.stringify(items);
+}else{
+JSON.stringify({url:u});
+}
