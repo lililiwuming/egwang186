9d53b9e4cef9a8572485d15e68ef0e3f20a28c18
1260957445@qq.com <1260957445@qq.com>
2022-03-13 19:08:32 +0800

    g

--- js3.0/总代码.js
+++ js3.0/总代码.js
@@ -291,8 +291,7 @@ _.read(filename);
 eval(e2Rex(getHttp('https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/aliyun/QJS.js'),'.dn64()'));
 var filename='一个影视本地规则.txt';
 var 记录=[];
-if(getVar("KEY")!='null'){
-    alert(getVar("KEY"));
+if(getVar("KEY")!='null'||getVar("KEY")!=''){
 var data=JSON.parse(getVar("KEY"));
 if(data.title&&data.url&&data.img){
 var title=data.title;var baseURL=data.url;var img=data.img;
