f806c11858b5420e3eee211eeef67109a24ebf58
1260957445@qq.com <1260957445@qq.com>
2022-04-02 11:42:45 +0800

    g

--- aliyun/总代码.js
+++ aliyun/总代码.js
@@ -1,6 +1,5 @@
 ######share_token1
-alert("@"+getVar("url")+"@")
-if(typeof(getVar("url"))!="undefined"&&getVar("url")!="null"){
+if(getVar("url").length>1&&getVar("url")!="null"){
 if(getVar("url").indexOf("aliyundrive.com/s/")!=-1||getVar("url").indexOf("share_id-")!=-1){
     if(getVar("url").indexOf("aliyundrive.com/s/")!=-1){
     var share_id=getVar("url").match(/\.com\/s\/([0-9a-zA-Z]+)/)[1];
