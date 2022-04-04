5a53e3582717c0b20088010b45fb6869ea323345
1260957445@qq.com <1260957445@qq.com>
2022-04-04 15:49:22 +0800

    g

--- oneiptv/备用直播源规则.js
+++ oneiptv/备用直播源规则.js
@@ -19,7 +19,7 @@
     {
         "title":"biubiu自建",
         "rule":{
-            "分类":'var a=e2Arr(getHttp(JSON.stringify({url:"https://gitee.com/likefree1/universe/raw/master/biubiu自建.html"})),".z(#[^,#]+?头#[\\\\s\\\\S]+?#[^,#]+?尾#)");var items=[];for(var i in a){var title=e2Rex(a[i],".z2(#\\\\([^,#]+?\\\\)头#)");var ul=a[i];var url="q:播放器?url="+title;items.push({title:title,ul:ul,url:url});}JSON.stringify(items);',
+            "分类":'var a=e2Arr(getHttp(JSON.stringify({url:"https://gitee.com/likefree1/universe/raw/master/biubiu/zb.php"})),".z(#[^,#]+?头#[\\\\s\\\\S]+?#[^,#]+?尾#)");var items=[];for(var i in a){var title=e2Rex(a[i],".z2(#\\\\([^,#]+?\\\\)头#)");var ul=a[i];var url="q:播放器?url="+title;items.push({title:title,ul:ul,url:url});}JSON.stringify(items);',
             "选集规则":'var 分类=e2Arr(getVar("CODE"),".json(ul)");var 列表规则=".z(.+?,.+)";var 线路="";var 标题规则=".ty(#).tz(头)";var 选集规则=".tz(,)";var 选集地址规则=".c(http://ip111.cn/?wd=).ty(,)";',
             "免嗅探规则":'var urls=getVar("url").split("?wd=")[1].split("#").filter(Boolean);var items=[];for(var i in urls){if(urls[i].indexOf(".php?")!=-1){var resp=JZ(JSON.stringify({url:urls[i],redirect:false}));var u=resp.head.Location||resp.head.location;if(!u){u=urls[i]+"&type=.m3u8";}}else{var u=urls[i];}items.push({url:u});}JSON.stringify(items);'
         }
