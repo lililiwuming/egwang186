27d58845a479f935dcc272150ad4e687e2b8f29c
1260957445@qq.com <1260957445@qq.com>
2022-04-12 23:01:40 +0800

    g

--- oneiptv/备用直播源规则.js
+++ oneiptv/备用直播源规则.js
@@ -3,7 +3,7 @@
     {
        "title":"推荐订阅",
        "rule":{
-            "分类":'var a="松鼠TV$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/松鼠TV.txt$$央卫0$https://cdn.jsdelivr.net/gh/wudongdefeng/za@master/wind/0.txt$$地方iptv$https://cdn.jsdelivr.net/gh/wudongdefeng/za@master/wind/iptv.txt$$六维liuwei$https://cdn.jsdelivr.net/gh/wudongdefeng/za@master/wind/liuwei.txt$$七彩直播$http://82.156.222.77/iptv/tv.txt$$TVFIX综合直播$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/TVFIX综合直播.txt$$TVMARK综合直播$http://fangsong.live/iptv/tv.txt$$镇弟直播$https://wds.ecsxs.com/223209.js$$综合直播$http://139.9.166.60/iptv/CR.txt$$晴天直播$https://wds.ecsxs.com/221609.json$$银河综合$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/银河综合.txt$$六维修改版$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/六维修改版.txt$$台湾台$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/台湾台.txt$$IPTVMEDIA-Adult$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/IPTVMEDIA-ADULT.txt$$Royal-Adult$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/Royal-Adult.txt";var a=a.split("$$");var items=[];for(var i in a){var title=a[i].split("$")[0];var url=a[i].split("$")[1];items.push({title:title,url:"q:自定义播放器?url=远程$$"+url});}JSON.stringify(items);',
+            "分类":'var a="松鼠TV$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/松鼠TV.txt$$央卫0$https://cdn.jsdelivr.net/gh/wudongdefeng/za@master/wind/0.txt$$地方iptv$https://cdn.jsdelivr.net/gh/wudongdefeng/za@master/wind/iptv.txt$$六维liuwei$https://cdn.jsdelivr.net/gh/wudongdefeng/za@master/wind/liuwei.txt$$七彩直播$http://82.156.222.77/iptv/tv.txt$$TVFIX综合直播$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/TVFIX综合直播.txt$$TVMARK综合直播$http://fangsong.live/iptv/tv.txt$$省市地方$https://wds.ecsxs.com/223218.js$$综合直播$http://139.9.166.60/iptv/CR.txt$$晴天直播$https://wds.ecsxs.com/221609.json$$银河综合$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/银河综合.txt$$六维修改版$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/六维修改版.txt$$台湾台$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/台湾台.txt$$IPTVMEDIA-Adult$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/IPTVMEDIA-ADULT.txt$$Royal-Adult$https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zby/txt源/Royal-Adult.txt";var a=a.split("$$");var items=[];for(var i in a){var title=a[i].split("$")[0];var url=a[i].split("$")[1];items.push({title:title,url:"q:自定义播放器?url=远程$$"+url});}JSON.stringify(items);',
             "选集规则":'',
             "免嗅探规则":''
         }
@@ -20,6 +20,14 @@
         "title":"biubiu自建",
         "rule":{
             "分类":'var a=e2Arr(getHttp(JSON.stringify({url:"https://gitee.com/likefree1/universe/raw/master/zz/zb.php"})),".z(#[^,#]+?头#[\\\\s\\\\S]+?#[^,#]+?尾#)");var items=[];for(var i in a){var title=e2Rex(a[i],".z2(#\\\\([^,#]+?\\\\)头#)");var ul=a[i];var url="q:播放器?url="+title;items.push({title:title,ul:ul,url:url});}JSON.stringify(items);',
+            "选集规则":'var 分类=e2Arr(getVar("CODE"),".json(ul)");var 列表规则=".z(.+?,.+)";var 线路="";var 标题规则=".ty(#).tz(头)";var 选集规则=".tz(,)";var 选集地址规则=".c(http://ip111.cn/?wd=).ty(,)";',
+            "免嗅探规则":'var urls=getVar("url").split("?wd=")[1].split("#").filter(Boolean);var items=[];for(var i in urls){if(urls[i].indexOf(".php?")!=-1){var resp=JZ(JSON.stringify({url:urls[i],redirect:false}));var u=resp.head.Location||resp.head.location;if(!u){u=urls[i]+"&type=.m3u8";}}else{var u=urls[i];}items.push({url:u});}JSON.stringify(items);'
+        }
+    },
+    {
+        "title":"biubiu-1",
+        "rule":{
+            "分类":'var a=e2Arr(getHttp(JSON.stringify({url:"https://wds.ecsxs.com/223236.js"})),".z(#[^,#]+?头#[\\\\s\\\\S]+?#[^,#]+?尾#)");var items=[];for(var i in a){var title=e2Rex(a[i],".z2(#\\\\([^,#]+?\\\\)头#)");var ul=a[i];var url="q:播放器?url="+title;items.push({title:title,ul:ul,url:url});}JSON.stringify(items);',
             "选集规则":'var 分类=e2Arr(getVar("CODE"),".json(ul)");var 列表规则=".z(.+?,.+)";var 线路="";var 标题规则=".ty(#).tz(头)";var 选集规则=".tz(,)";var 选集地址规则=".c(http://ip111.cn/?wd=).ty(,)";',
             "免嗅探规则":'var urls=getVar("url").split("?wd=")[1].split("#").filter(Boolean);var items=[];for(var i in urls){if(urls[i].indexOf(".php?")!=-1){var resp=JZ(JSON.stringify({url:urls[i],redirect:false}));var u=resp.head.Location||resp.head.location;if(!u){u=urls[i]+"&type=.m3u8";}}else{var u=urls[i];}items.push({url:u});}JSON.stringify(items);'
         }
