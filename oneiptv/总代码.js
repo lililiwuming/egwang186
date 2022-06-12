e3e40729b4598954ebda220c3764b57bfb4d5944
1260957445@qq.com <1260957445@qq.com>
2022-06-12 11:45:13 +0800

    g

--- oneiptv/总代码.js
+++ oneiptv/总代码.js
@@ -131,12 +131,8 @@ if(code.indexOf("#genre#")!=-1){
     var res={};var items=[];
 for(var i in code){
     var 选集=code[i].match(/,(.+)/)[1];var 选集地址=code[i].match(/,.+[\s]+?(.+)/)[1];
-    if(code[i].search(/group-title=".+?",/)!=-1){
-        if(code[i].match(/group-title="(.+)",/)){
-        var type=code[i].match(/group-title="(.+)",/)[1];
-        }else{
-            var type="分类不规范";
-        }
+    if(code[i].search(/group-title=".*?",/)!=-1){
+        var type=code[i].match(/group-title="(.*?)",/)[1];
     }else{
         var type="未分类";
     }
